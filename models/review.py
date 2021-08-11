1b612c36f614dbc02e50d8ddc07cd2cb789e5b17
Philimon Derib <philimonderib2019@gmail.com>
2021-08-11 11:16:59 +0300

    editied

--- models/review.py
+++ models/review.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel, Base
 from models.place import Place
-from models.user import User
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine, Column, Integer, String
 
@@ -11,6 +10,6 @@ class Review(BaseModel, Base):
         Review class
     """
     __tablename__ = "reviews"
-    place_id = Column(String(60), nullable=False, ForigenKey(Place.id))
-    user_id = Column(String(60), nullable=False, ForigenKey(User.id))
+    # place_id = Column(String(60), nullable=False, ForigenKey(Place.id))
+    # user_id = Column(String(60), nullable=False, ForigenKey(User.id))
     text = Column(String(1024), nullable=False)
--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
     module containing user class
 """
 from models.base_model import BaseModel, Base
-from models.review import Review
+# from models.review import Review
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine, Column, Integer, String
 
@@ -16,4 +16,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    reviews = relationship("Review", backref="user")
+    # reviews = relationship("Review", backref="user")
